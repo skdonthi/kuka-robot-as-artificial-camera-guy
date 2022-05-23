953621da97360c42b7b77fdb6078843e982aed40
skdonthi <skdonthi@outlook.de>
2022-05-23 12:28:54 +0200

    yielding camera face and depth info to the main

--- kinect_openni.py
+++ kinect_openni.py
@@ -5,45 +5,54 @@ from openni import openni2
 import numpy as np
 import cv2
 
-openni2.initialize() # can also accept the path of the OpenNI redistribution
 
-dev = openni2.Device.open_any()
-print(dev.get_device_info())
-#print(dev.get_sensor_info())
+class StaticPositionedCamera:
+    def __init__(self):
+        openni2.initialize() # can also accept the path of the OpenNI redistribution
+        dev = openni2.Device.open_any()
+        print(dev.get_device_info())        
+        self.depth_stream = dev.create_depth_stream()
+        self.depth_stream.start()
+        self.color_stream = dev.create_color_stream()
+        self.color_stream.start()
+        self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+        self.eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
+        self.key = None
+        
+    def get_face_and_depth_info(self):        
+        info = []
+        depth_frame = self.depth_stream.read_frame()
+        depth_data = depth_frame.get_buffer_as_uint16()
+        depth_img1 = np.ndarray((depth_frame.height, depth_frame.width), dtype=np.uint16, buffer=depth_data).astype(np.float32)
+        depth_img = depth_img1/1000.0 # convert to meters 
+        cv2.imshow("Depth", depth_img-.5) # uniform shift by 0.5 m distance for better display
+        # print('start=', depth_img[depth_frame.height//2,depth_frame.width//2]) ## access one distance sample from the middle
+        color_frame = self.color_stream.read_frame()
+        color_data = color_frame.get_buffer_as_uint8()
+        color_img = np.ndarray((color_frame.height, color_frame.width, 3), dtype=np.uint8, buffer=color_data)  
 
-depth_stream = dev.create_depth_stream()
-depth_stream.start()
-color_stream = dev.create_color_stream()
-color_stream.start()
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-key = None
-while key != ord('q'):
-    depth_frame = depth_stream.read_frame()
-    depth_data = depth_frame.get_buffer_as_uint16()
-    depth_img1 = np.ndarray((depth_frame.height, depth_frame.width), dtype=np.uint16, buffer=depth_data).astype(np.float32)
-    depth_img = depth_img1/1000.0 # convert to meters 
-    cv2.imshow("Depth", depth_img-.5) # uniform shift by 0.5 m distance for better display
-    # print('start=', depth_img[depth_frame.height//2,depth_frame.width//2]) ## access one distance sample from the middle
-    color_frame = color_stream.read_frame()
-    color_data = color_frame.get_buffer_as_uint8()
-    color_img = np.ndarray((color_frame.height, color_frame.width, 3), dtype=np.uint8, buffer=color_data)  
-
-    # Convert to grayscale
-    gray = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
-    # Detect the faces
-    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    # Draw the rectangle around each face
-    for (x, y, w, h) in faces:
-        cv2.rectangle(color_img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-        height = int(y+(h/2))
-        width = int(x+(w/2))
-        depth = depth_img[height, width]
-        print('end=', depth)
-    # Display
-    cv2.imshow("Color", color_img[...,::-1])
-    key = cv2.waitKey(1) & 0xFF
-
-depth_stream.stop()
-color_stream.stop()
-
-openni2.unload()
+        # Convert to grayscale
+        gray = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
+        # Detect the faces
+        faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
+        # Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(color_img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+            roi_gray = gray[y:y+h, x:x+w]
+            roi_color = color_img[y:y+h, x:x+w]
+            eyes = self.eye_cascade.detectMultiScale(roi_gray)
+            for (ex,ey,ew,eh) in eyes:
+                cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+            height = int(y+(h/2))
+            width = int(x+(w/2))
+            depth = depth_img[height, width]
+            info.extend([height, width, depth])
+            print(len(info))
+        # Display
+        cv2.imshow("Color", color_img[...,::-1])
+        yield info
+        
+    def close_frames():
+        self.depth_stream.stop()
+        self.color_stream.stop()
+        openni2.unload()
--- main.py
+++ main.py
@@ -1,32 +1,27 @@
 from camera import face_detection
 from robot import RemoteControlKUKA
+from kinect_openni import StaticPositionedCamera
 import time
+import cv2
 
-   
+
 if __name__ == '__main__':
-    rck = RemoteControlKUKA()
+    key = None
+    camera = StaticPositionedCamera()
+    # rck = RemoteControlKUKA()
     x=0
     z=100
     home = '{E6AXIS: A1 0, A2 -90.0, A3 90.0, A4 0.0, A5 -90.0, A6 270.0, E1 0.0, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}'
-    rck.moveTo(home)
+    # rck.moveTo(home)
+    while key != ord('q'):
+        for info in camera.get_face_and_depth_info():
+            print('info = ', info)
+            # e6axis2 = '{E6AXIS: A1 '+str(x)+', A2 -90.0, A3 '+str(z)+', A4 0.0, A5 -90.0, A6 270.0, E1 0.0, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}'
+            key = cv2.waitKey(1) & 0xFF
+            # rck.moveTo(e6axis2)
+        
+    camera.close_frames()
 
-    while True:
-        for position in face_detection():
-            print("main")
-            print(position)
-            
-            x = x+(position[0]-320)/12
-            # z = 100-((position[1]-225)/8.75)
-            z = max(min(100-((position[1]-225)/8.75), 120), 70)
-            e6axis1 = '{E6AXIS: A1 '+str(x)+', A2 -90.0, A3 90.0, A4 0.0, A5 -90.0, A6 270.0, E1 0.0, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}'
-            e6axis2 = '{E6AXIS: A1 '+str(x)+', A2 -90.0, A3 '+str(z)+', A4 0.0, A5 -90.0, A6 270.0, E1 0.0, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}'
-            
-            # e6axis1 = '{E6AXIS: A1 '+str(x)+', A2 -90.0, A3 '+str(z)+', A4 0.0, A5 -90.0, A6 0.0, E1 0.0, E2 0.0, E3 0.0, E4 0.0, E5 0.0, E6 0.0}'
 
-            # z = 887.51    
-            # rck.move_lin_e6pos('{X 425.297516,Y '+str(y)+',Z '+str(z)+',A -179.973938,B 67.7722855,C -179.862015,S 6,T 27,E1 0.0,E2 0.0,E3 0.0,E4 0.0,E5 0.0,E6 0.0}')
-            print("----------------------")
-            print(e6axis2)
-            # time.sleep(5)
-            print(position)
-            rck.moveTo(e6axis2)
+
+
